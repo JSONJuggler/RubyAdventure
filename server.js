260d89d4490bcad7a23cc4c3028f917e9123bb62
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-30 02:12:36 -0500

    old comments

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// Set static folder
 app.use(express.static("public"));
 
-// Serve index.html on all routes except the api routes above
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
